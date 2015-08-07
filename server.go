016d62178b3d802ec7b195d0f4647f61dab12229
Lucas Simonelli <lucas.simonelli@fdvsolutions.com>
2015-08-07 18:45:04 -0300

    A couple more methods

--- server.go
+++ server.go
@@ -1,11 +1,21 @@
 package main
 
-import "github.com/go-martini/martini"
+import (
+  "net/http"
+  "github.com/go-martini/martini"
+)
 
 func main() {
   m := martini.Classic()
   m.Get("/", func() string {
     return "Hello world!"
+  })
+  m.Get("/error", func() (int, string) {
+  return 400, "i'm a teapot" // HTTP 418 : "i'm a teapot"
+  })
+
+  m.Get("/params", func(res http.ResponseWriter, req *http.Request) { // res and req are injected by Martini
+  res.WriteHeader(200) // HTTP 200
   })
   m.Run()
 }
